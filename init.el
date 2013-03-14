6b05dcd7a79005b30b00f86c1a6e79338c144d14
Alex Malozemoff <amalozemoff1@gmail.com>
2013-03-14 00:59:49 -0400

    added erc config

--- init.el
+++ init.el
@@ -3,11 +3,17 @@
 
 (add-to-list 'load-path "~/.emacs.d")
 (add-to-list 'load-path "~/.emacs.d/elisp")
+;; (add-to-list 'load-path "~/.emacs.d/vendor/erc-sasl")
 
 (setq custom-file "~/.emacs.d/config/custom.el")
 (when (file-exists-p custom-file) (load custom-file))
 
+(load "config/private" t)
 (load "config/packages")
+; turn on flymake for all applicable file types
+;; (require 'flymake)
+;; (add-hook 'find-file-hook 'flymake-find-file-hook)
+
 (load "config/ui")
 (load "config/funs")
 (load "config/mode")
@@ -17,6 +23,7 @@
 
 ; specific modules
 (load "config/email")
+(load "config/erc")
 (load "config/latex")
 (load "config/ocaml")
 (load "config/ruby")
