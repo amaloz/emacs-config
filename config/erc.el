d266a9f4e073869eaba1b747616130e97e8e1eed
Alex Malozemoff <amalozemoff1@gmail.com>
2013-03-16 12:11:36 -0400

    make erc more user-configurable

--- config/erc.el
+++ config/erc.el
@@ -4,8 +4,17 @@
 (require 'tls)
 (setq tls-program '("gnutls-cli --starttls -p %p %h"))
 
-;;; Note: need to define private-erc-nick, private-erc-user-full-name,
-;;; private-erc-user-information, and private-erc-system-name.
+;;
+;; Note: need to define the following variables:
+;;
+;;   private-erc-nick
+;;   private-erc-user-full-name
+;;   private-erc-user-information
+;;   private-erc-system-name
+;;   private-erc-default-server
+;;   private-erc-default-port
+;;   private-erc-rooms
+;;
 (eval-after-load "erc"
   '(progn
      (if (boundp 'private-erc-nick)
@@ -16,8 +25,10 @@
          (setq erc-user-information private-erc-user-information))
      (if (boundp 'private-erc-system-name)
          (setq erc-system-name private-erc-system-name))
-     (setq erc-default-server "irc.freenode.net")
-     (setq erc-default-port "7000")
+     (if (boundp 'private-erc-default-server)
+         (setq erc-default-server private-erc-default-server))
+     (if (boundp 'private-erc-default-port)
+         (setq erc-default-port private-erc-default-port))
      (setq erc-anonymous-login t)
      (setq erc-auto-query 'window-noselect)
      (setq erc-modules '(autojoin button completion fill irccontrols keep-place
@@ -47,7 +58,6 @@
      (setq erc-server-reconnect-attempts 4
            erc-server-reconnect-timeout 5)
      (setq erc-keywords nil)
-
      ))
 
 (defun amaloz/start-irc ()
@@ -58,12 +68,16 @@
            :nick erc-nick
            :full-name erc-user-full-name))
 
-;; from http://emacs-fu.blogspot.com/2009/06/erc-emacs-irc-client.html
-(defun amaloz/erc-start-or-switch ()
-  "Connect to ERC, or switch to last active buffer"
-  (interactive)
-  (if (get-buffer "irc.freenode.net:7000")
-      (erc-track-switch-buffer 1)
-    (when (y-or-n-p "Start ERC? ")
-      (amaloz/start-irc))))
-(define-key global-map (kbd "C-c e") 'amaloz/erc-start-or-switch)
+(if (and (boundp 'private-erc-default-server) (boundp 'private-erc-default-port))
+    ;; modified from http://emacs-fu.blogspot.com/2009/06/erc-emacs-irc-client.html
+    (defun amaloz/erc-start-or-switch ()
+      "Connect to ERC, or switch to last active buffer"
+      (interactive)
+      (if (get-buffer
+           (concat private-erc-default-server ":" private-erc-default-port))
+          (erc-track-switch-buffer 1)
+        (when (y-or-n-p "Start ERC? ")
+          (amaloz/start-irc)))))
+
+(if (fboundp 'amaloz/erc-start-or-switch)
+    (define-key global-map (kbd "C-c e") 'amaloz/erc-start-or-switch))
