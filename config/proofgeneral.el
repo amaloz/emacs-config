78b69e53544488acee446837e338f8aa85615dfa
Alex Malozemoff <amalozemoff1@gmail.com>
2013-04-04 23:35:12 -0400

    proofgeneral: automatically compile required libs

--- config/proofgeneral.el
+++ config/proofgeneral.el
@@ -1,2 +1,11 @@
 
 (load-file "/usr/share/emacs/site-lisp/ProofGeneral/generic/proof-site.el")
+
+(defun amaloz/coq-mode-defaults ()
+  (setq coq-compile-before-require t))
+
+(setq amaloz/coq-mode-hook 'amaloz/coq-mode-defaults)
+
+(add-hook 'coq-mode-hook
+          (lambda ()
+            (run-hooks 'amaloz/coq-mode-hook)))
