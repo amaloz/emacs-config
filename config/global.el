4b445e669497464fe0c8e7c84e3b427f11ec5fde
amaloz <amalozemoff1@gmail.com>
2013-09-04 10:39:28 -0400

    added dired-single and dired+

--- config/global.el
+++ config/global.el
@@ -1,7 +1,35 @@
 ;; global configuration
 ;; 02 March 2013
 
-(amaloz/ensure-module-deps '(move-text pretty-mode-plus))
+(amaloz/ensure-module-deps
+ '(dired+ dired-single move-text pretty-mode-plus))
+
+(require 'dired-single)
+(require 'dired+)
+(defun my-dired-init ()
+  "Bunch of stuff to run for dired, either immediately or when it's
+   loaded."
+  ;; <add other stuff here>
+  (define-key dired-mode-map [return] 'dired-single-buffer)
+  (define-key dired-mode-map [mouse-1] 'dired-single-buffer-mouse)
+  (define-key dired-mode-map "^"
+    (function
+     (lambda nil (interactive) (dired-single-buffer "..")))))
+;; if dired's already loaded, then the keymap will be bound
+(if (boundp 'dired-mode-map)
+    ;; we're good to go; just add our bindings
+    (my-dired-init)
+  ;; it's not loaded yet, so add our bindings to the load-hook
+  (add-hook 'dired-load-hook 'my-dired-init))
+
+(global-set-key [(f5)] 'dired-single-magic-buffer)
+(global-set-key [(control f5)] (function
+    (lambda nil (interactive)
+        (dired-single-magic-buffer default-directory))))
+(global-set-key [(shift f5)] (function
+    (lambda nil (interactive)
+        (message "Current directory is: %s" default-directory))))
+(global-set-key [(meta f5)] 'dired-single-toggle-buffer-name)
 
 (require 'move-text)
 (move-text-default-bindings)
