a19dfe5c7cc8a4fe36e50507b5831edeff804be0
amaloz <amaloz@cs.umd.edu>
2014-10-13 10:09:19 -0400

    modifications to c init script

--- config/c.el
+++ config/c.el
@@ -1,14 +1,37 @@
 ;; C config
 ;; 12 November 2013
 
-(amaloz/ensure-module-deps '())
+(amaloz/ensure-module-deps '(auto-complete-clang ecb yasnippet))
 
-(defun amaloz/c-mode-defaults ()
-  (setq c-basic-offset 4)
-  (setq show-trailing-whitespace t))
+(require 'cc-mode)
+(require 'yasnippet)
+(yas-global-mode 1)
 
-(setq amaloz/c-mode-hook 'amaloz/c-mode-defaults)
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+(ac-set-trigger-key "TAB")
+(ac-set-trigger-key "<tab>")
 
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (run-hooks 'amaloz/c-mode-hook)))
+(require 'auto-complete-clang)
+(define-key c++-mode-map (kbd "C-S-<return>") 'ac-complete-clang)
+
+(require 'ecb)
+(require 'ecb-autoloads)
+(setq ecb-layout-name "right1")
+(setq ecb-show-sources-in-directories-buffer 'always)
+(setq ecb-compile-window-height 12)
+
+(setq-default c-basic-offset 4 c-default-style "linux")
+(setq-default tab-width 4 indent-tabs-mode t)
+(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
+
+;; (defun amaloz/c-mode-defaults ()
+;;   (setq c-basic-offset 4 c-default-style "linux")
+;;   (setq show-trailing-whitespace t))
+
+;; (setq amaloz/c-mode-hook 'amaloz/c-mode-defaults)
+
+;; (add-hook 'c-mode-common-hook
+;;           (lambda ()
+;;             (run-hooks 'amaloz/c-mode-hook)))
