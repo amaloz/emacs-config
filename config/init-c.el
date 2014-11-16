3374d049fd779dbf7a17d0181666ab9077136e0d
Alex Malozemoff <amalozemoff1@gmail.com>
2014-11-16 12:41:21 -0500

    update c initialization

--- config/init-c.el
+++ config/init-c.el
@@ -5,17 +5,18 @@
 
 (require 'cc-mode)
 
+(setq-default c-basic-offset 4 c-default-style "linux")
+(setq-default tab-width 4 indent-tabs-mode nil)
+
 (after-load
- 'c-mode
- (require 'ecb)
- (require 'ecb-autoloads)
- (setq ecb-layout-name "right1")
- (setq ecb-show-sources-in-directories-buffer 'always)
- (setq ecb-compile-window-height 12)
- (setq-default c-basic-offset 4 c-default-style "linux")
- (setq-default tab-width 4 indent-tabs-mode t)
- (define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
- (require 'auto-complete-clang)
- (define-key c++-mode-map (kbd "C-S-<return>") 'ac-complete-clang)
- )
+  'c-mode
+  (require 'ecb)
+  (require 'ecb-autoloads)
+  (setq ecb-layout-name "right1")
+  (setq ecb-show-sources-in-directories-buffer 'always)
+  (setq ecb-compile-window-height 12)
+  (define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
+  (require 'auto-complete-clang)
+  (define-key c++-mode-map (kbd "C-S-<return>") 'ac-complete-clang)
+  )
 
